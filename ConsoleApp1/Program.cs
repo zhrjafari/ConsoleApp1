539ee241a6ffebd4e17c44eb32bf6d3174ee8992
zahrajafari <jafari_zhr@yahoo.com>
2023-02-25 23:01:21 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace project1
+{
+    class Test
+    {
+        public static void Main()
+        {
+            string input_1 = Console.ReadLine();
+            string input_2 = Console.ReadLine();
+            int num1 = Convert.ToInt32(input_1);
+            int num2 = Convert.ToInt32(input_2);
+            int sum = num1 + num2;
+            Console.WriteLine(input_1 + " + " + input_2 + " = " + sum);
+        }
+    }
+}
